ed62b7e04e9593d2587f01f2babf34176dfefcd0
jay.develops <swejesusperez@gmail.com>
2025-08-25 20:23:52 -0700

    Removed preview from admin entirely.

--- config/admin.ts
+++ config/admin.ts
@@ -17,43 +17,4 @@ export default ({ env }) => ({
     nps: env.bool("FLAG_NPS", true),
     promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
-  preview: {
-    enabled: true,
-    config: {
-      allowedOrigins: [env("CLIENT_URL")],
-      async handler(uid, { documentId, locale, status }) {
-        const document = await strapi.documents(uid).findOne({
-          documentId,
-          populate: ["title"],
-          fields: ["title", "slug"],
-        });
-        const slug =
-          document.slug ||
-          document.title
-            .toString()
-            .toLowerCase()
-            .trim()
-            .replace(/\s+/g, "-")
-            .replace(/&/g, "-and-")
-            .replace(/[^\w-]+/g, "")
-            .replace(/--+/g, "-");
-
-        if (!slug) {
-          throw new Error("Unable to generate slug for preview");
-        }
-
-        const urlSearchParams = new URLSearchParams({
-          secret: env("PREVIEW_SECRET"),
-          ...(slug && { slug }),
-          locale,
-          uid,
-          status,
-        });
-
-        const previewURL = `${env("CLIENT_URL")}/api/preview?${urlSearchParams}`;
-
-        return previewURL;
-      },
-    },
-  },
 });
